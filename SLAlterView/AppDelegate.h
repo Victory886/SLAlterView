46bc430619931a254e8521635b5fe8bfab26cec9
Terry <baosl_886@126.com>
2016-05-03 08:49:31 +0800

    Initial Commit

---
+++ SLAlterView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLAlterView
+//
+//  Created by 收付宝－胜利 on 16/5/3.
+//  Copyright © 2016年 胜利_Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
